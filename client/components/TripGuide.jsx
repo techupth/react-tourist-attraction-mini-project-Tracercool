4d5e65ef0d4a7a0f81b211d060f8129eabb1cab2
Mon <tracer.coolkid@gmail.com>
2024-06-12 11:08:14 +0700

    style

---
+++ client/components/TripGuide.jsx
@@ -0,0 +1,173 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+import React from "react";
+
+function TripGuide(props) {
+  let newDescription = "";
+  if (props.description.length > 100) {
+    newDescription = props.description.slice(0, 100);
+  } else {
+    newDescription = props.description;
+  }
+  return (
+    <div
+      className="block-travel"
+      css={css`
+        display: flex;
+        flex-direction: row;
+        width: 80vw;
+        height: 250px;
+        box-sizing: border-box;
+        margin: 0px;
+        gap: 40px;
+        padding: 100px;
+      `}
+    >
+      <section
+        className="main-picture"
+        css={css`
+          display: flex;
+          flex: 2;
+          justify-content: center;
+          align-items: center;
+        `}
+      >
+        <img
+          src={props.extraPhoto}
+          css={css`
+            display: inline;
+            width: 250px;
+            height: 200px;
+            border-radius: 30px;
+          `}
+        ></img>
+      </section>
+      <section
+        className="details"
+        css={css`
+          display: flex;
+          flex: 8;
+          flex-direction: column;
+          justify-content: center;
+          gap: 5px;
+        `}
+      >
+        <h3
+          css={css`
+            margin: 0px;
+            font-family: "Prompt";
+            font-size: 23px;
+          `}
+        >
+          {props.title}
+        </h3>
+        <p
+          css={css`
+            margin: 0px;
+            font-size: 15px;
+            color: #838383;
+            font-family: "Prompt";
+          `}
+        >
+          {newDescription}
+        </p>
+        <a
+          href={props.url}
+          target="_blank"
+          css={css`
+            margin: 0px;
+            font-size: 15px;
+            color: #00ccff;
+            width: 50px;
+            font-family: "Prompt";
+          `}
+        >
+          อ่านต่อ
+        </a>
+        <div
+          css={css`
+            display: flex;
+            gap: 10px;
+          `}
+        >
+          <span
+            css={css`
+              margin: 0px;
+              font-size: 13px;
+              color: #838383;
+              font-family: "Prompt";
+            `}
+          >
+            หมวด
+          </span>
+          {props.tag.map((item, index, array) => {
+            return index == array.length - 1 ? (
+              <>
+                <spa
+                  css={css`
+                    margin: 0px;
+                    font-size: 13px;
+                    color: #838383;
+                    font-family: "Prompt";
+                  `}
+                >
+                  และ
+                </spa>
+                <a
+                  key={index}
+                  css={css`
+                    margin: 0px;
+                    font-size: 13px;
+                    text-decoration: underline;
+                    color: #838383;
+                    font-family: "Prompt";
+                  `}
+                >
+                  {item}
+                </a>
+              </>
+            ) : (
+              <a
+                key={index}
+                css={css`
+                  margin: 0px;
+                  font-size: 13px;
+                  text-decoration: underline;
+                  color: #838383;
+                  font-family: "Prompt";
+                `}
+              >
+                {item}
+              </a>
+            );
+          })}
+        </div>
+        <div
+          css={css`
+            display: flex;
+            gap: 20px;
+          `}
+        >
+          {props.photos.map((item, index) => {
+            return (
+              <img
+                key={index}
+                src={item}
+                css={css`
+                  display: inline;
+                  width: 75px;
+                  height: 75px;
+                  box-sizing: border-box;
+                  border-radius: 10px;
+                  margin-top: 10px;
+                `}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default TripGuide;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,116 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
 import "./App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TripGuide from "../components/TripGuide";
 
 function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+  const [listTripGuide, setListTripGuide] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const getDataTripGuide = async (text) => {
+    let result = await axios.get(
+      "http://localhost:4001/trips?keywords=" + text
+    );
+    setListTripGuide(result.data.data);
+  };
+  const handleSearch = async (event) => {
+    let text = event.target.value;
+    setSearchText(text);
+    getDataTripGuide(text);
+  };
+
+  useEffect(() => {
+    getDataTripGuide(searchText);
+  }, [searchText]);
+
+  return (
+    <div className="App">
+      <div
+        className="header"
+        css={css`
+          font-family: "Prompt";
+          display: flex;
+          flex-direction: column;
+        `}
+      >
+        <h1
+          css={css`
+            color: #318ce7;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            font-size: 60px;
+          `}
+        >
+          เที่ยวไหนดี
+        </h1>
+        <p
+          css={css`
+            display: flex;
+            width: 80vw;
+            margin-left: 25vw;
+          `}
+        >
+          ค้นหาที่เที่ยว
+        </p>
+        <div
+          className="header-search-block"
+          css={css`
+            display: flex;
+            justify-content: center;
+            margin-bottom: 10px;
+          `}
+        >
+          <input
+            onChange={handleSearch}
+            placeholder="หาที่เที่ยวแล้วไปกัน .."
+            value={searchText}
+            css={css`
+              border: none;
+              &::placeholder {
+                color: #318ce7;
+            }
+              &:focus {
+                outline: 0px;
+              }
+            `}
+          ></input>
+        </div>
+
+        <hr
+          css={css`
+            width: 50vw;
+            color: black;
+          `}
+        ></hr>
+      </div>
+      <section
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        `}
+      >
+        {listTripGuide.map((item) => {
+          return (
+            <TripGuide
+              key={item.eid}
+              extraPhoto={item.photos.slice(0, 1)}
+              title={item.title}
+              description={item.description}
+              url={item.url}
+              tag={item.tags}
+              photos={item.photos.slice(1)}
+            />
+          );
+        })}
+      </section>
+    </div>
+  );
 }
 
 export default App;
